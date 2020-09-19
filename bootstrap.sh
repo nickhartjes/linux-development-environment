e8d83066ab77212f5eb9d2ba9ba5b8a4a6e58c50
Nick Hartjes <info@nickhartjes.nl>
2020-09-19 21:50:35 +0200

    First setup

---
+++ bootstrap.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+if [ ! "$HOME" == "$PWD" ]; then
+  echo "This script is intended to be run from the user's home path: $HOME"
+  exit 1
+fi
+
+
+# DEFAULTS
+BRANCH="master"
+ANSIBLE_ARGS=""
+
+# Use -gt 1 to consume two arguments per pass in the loop (e.g. each
+# argument has a corresponding value to go with it).
+# Use -gt 0 to consume one or more arguments per pass in the loop (e.g.
+# some arguments don't have a corresponding value to go with it such
+# as in the --default example).
+# note: if this is set to -gt 0 the /etc/hosts part is not recognized ( may be a bug )
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -b|--branch)
+    BRANCH="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+# BOOTSTRAP
+
+# Upgrade packages
+sudo apt-get update
+sudo apt-get --assume-yes upgrade
+
+# Install Ansible & Git
+sudo apt-get --assume-yes install ansible
+sudo apt-get --assume-yes install git
+
+# Clone dev-env repo if not already present
+if [ ! -d ".dev-env" ]; then
+  git clone https://github.com/nickhartjes/linux-development-environment.git .dev-env
+fi
+
+Checkout specified branch
+cd .dev-env
+git checkout ${BRANCH}
+
+Run Ansible playbook
+./install.sh
---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ansible-playbook -K -c local -i localhost install.yml
